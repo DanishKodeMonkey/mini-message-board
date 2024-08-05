ca6bbc68200ca0455ef276a598ba13860dc78c90
danishkodemonkey <humberjini@hotmail.com>
2024-08-05 15:23:40 +0200

    feat: index messageboard display

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const path = require('node:path');
 
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.send('Hello world'));
+// messages array, pseudo database
+const messages = [
+    { text: 'Hi there!', user: 'Rick', added: new Date() },
+    { text: 'Hello world!', user: 'Danish Kode Monkey', added: new Date() },
+];
+
+app.get('/', (req, res) =>
+    res.render('index', { title: 'mini-messageboard', messages: messages })
+);
+app.get('/form', (req, res) => res.render('new', { title: 'New message' }));
 
 const PORT = 3000;
 app.listen(PORT, () =>
