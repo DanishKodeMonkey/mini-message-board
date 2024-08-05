8627da0ec01cb59644b0f5e7468e4338ce86fb61
danishkodemonkey <humberjini@hotmail.com>
2024-08-05 15:38:42 +0200

    feat: add new post functionality

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Parse form contents through middleware to req.body object.
+app.use(express.urlencoded({ extended: true }));
+
 // messages array, pseudo database
 const messages = [
     { text: 'Hi there!', user: 'Rick', added: new Date() },
@@ -14,8 +17,20 @@ const messages = [
 app.get('/', (req, res) =>
     res.render('index', { title: 'mini-messageboard', messages: messages })
 );
-app.get('/form', (req, res) => res.render('new', { title: 'New message' }));
+app.get('/form', (req, res) => res.render('form', { title: 'New message' }));
 
+app.post('/new', (req, res) => {
+    const message = {
+        user: req.body.user,
+        text: req.body.text,
+    };
+    messages.push({
+        text: message.text,
+        user: message.user,
+        added: new Date(),
+    });
+    res.redirect('/');
+});
 const PORT = 3000;
 app.listen(PORT, () =>
     console.log('Express initialized, listening on port ', PORT)
