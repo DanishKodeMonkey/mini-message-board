e4863935ccc3e68b1b3075891e83b430ae804c7f
danishkodemonkey <humberjini@hotmail.com>
2024-08-13 15:48:03 +0200

    refactored to new file structure

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('node:path');
 const port = process.env.PORT || 3000;
+const messagesRouter = require('./routes/messagesRouter');
 
 const app = express();
 
@@ -10,42 +11,7 @@ app.set('view engine', 'ejs');
 // Parse form contents through middleware to req.body object.
 app.use(express.urlencoded({ extended: true }));
 
-// messages array, pseudo database
-const messages = [
-    { text: 'Hi there!', user: 'Rick', added: new Date() },
-    { text: 'Hello world!', user: 'Danish Kode Monkey', added: new Date() },
-];
-
-app.get('/', (req, res) =>
-    res.render('index', { title: 'mini-messageboard', messages: messages })
-);
-
-app.get('/messages/:id', (req, res) => {
-    const message = messages[req.params.id];
-    if (message) {
-        res.render('messageDetails', {
-            title: 'Message Details',
-            message: message,
-        });
-    } else {
-        res.status(404).send('message not found');
-    }
-});
-
-app.get('/form', (req, res) => res.render('form', { title: 'New message' }));
-
-app.post('/new', (req, res) => {
-    const message = {
-        user: req.body.user,
-        text: req.body.text,
-    };
-    messages.push({
-        text: message.text,
-        user: message.user,
-        added: new Date(),
-    });
-    res.redirect('/');
-});
+app.use('/', messagesRouter);
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
